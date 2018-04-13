50a06bef478ab15bb7c8393059a2b00aff456f30
prawncrown <38025852+prawncrown@users.noreply.github.com>
2018-04-13 15:48:19 +0900

    change

--- js/sensorgetter.js
+++ js/sensorgetter.js
@@ -1,4 +1,7 @@
-
+$(function () {
+    window.addEventListener("devicemotion", devicemotionHandler);
+    window.addEventListener("deviceorientation", deviceorientationHandler);
+  });
 function devicemotionHandler(event){
       // 加速度X軸
       var x = event.acceleration.x;
@@ -6,20 +9,26 @@ function devicemotionHandler(event){
       var y = event.acceleration.y;
       // 加速度Z軸
       var z = event.acceleration.z;
-      //ジャイロX軸
-      var beta = event.beta;
-      // ジャイロY軸
-      var gamma = event.gamma;
-      // ジャイロZ軸
-      var alpha = event.alpha;
+
 
       var html = "";
       html += "X回転 : " + x + "<br>";
       html += "Y回転 : " + y + "<br>";
       html += 'Z回転 : ' + z;
       $("#accelero").html(html);
-      html += "BETA : " + beta + "<br>";
-      html += "GAMMA : " + gamma + "<br>";
-      html += 'ALPHA : ' + alpha;
-      $("#gyro").html(html);
+
+}
+function deviceorientationHandler(event){
+  //ジャイロX軸
+  var beta = event.beta;
+  // ジャイロY軸
+  var gamma = event.gamma;
+  // ジャイロZ軸
+  var alpha = event.alpha;
+  
+  var html = "";
+  html += "BETA : " + beta + "<br>";
+  html += "GAMMA : " + gamma + "<br>";
+  html += 'ALPHA : ' + alpha;
+  $("#gyro").html(html);
 }
